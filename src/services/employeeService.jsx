f90530a3fbb802241030cc2da94fe9cf0420da6c
Jake Littleton <jakelittleton30@gmail.com>
2026-06-05 09:16:28 -0500

    employee service updated

--- src/services/employeeService.jsx
+++ src/services/employeeService.jsx
@@ -1,19 +1,19 @@
 export const getAllEmployees = () => {
-    return fetch("http://localhost:8088/employees?_expand=user").then((res) => res.json())
+    return fetch("http://localhost:8000/employees?_expand=user").then((res) => res.json())
 }
 
 export const getSingleEmployee = (id) => {
-    return fetch(`http://localhost:8088/employees?userId=${id}&_expand=user&_embed=employeeTickets`).then((res) => res.json())
+    return fetch(`http://localhost:8000/employees?userId=${id}&_expand=user&_embed=employeeTickets`).then((res) => res.json())
 }
 
 export const getEmployeeByUserId = (id) => {
     return fetch(
-        `http://localhost:8088/employees?_expand=user&_embed=employeeTickets&userId=${id}`
+        `http://localhost:8000/employees?_expand=user&_embed=employeeTickets&userId=${id}`
     ).then((res) => res.json())
 }
 
 export const updateEmployee = (employee) => {
-    return fetch(`http://localhost:8088/employees/${employee.id}`, {
+    return fetch(`http://localhost:8000/employees/${employee.id}`, {
         method: "PUT",
         headers: {
             "Content-Type": "application/json",
