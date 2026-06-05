c437c861d434be52740c615e64eac4f30156b021
Jake Littleton <jakelittleton30@gmail.com>
2026-06-05 09:19:22 -0500

    refactoring userservice

--- src/services/userService.jsx
+++ src/services/userService.jsx
@@ -1,11 +1,11 @@
 export const getNonStaffUsers = () => {
-    return fetch("http://localhost:8088/users?isStaff=false").then((res) => 
+    return fetch("http://localhost:8000/users?isStaff=false").then((res) => 
         res.json()
 )
 }
 
 export const getStaffUsers = () => {
-    return fetch("http://localhost:8088/users?isStaff=true").then((res) => 
+    return fetch("http://localhost:8000/users?isStaff=true").then((res) => 
         res.json()
 )
 }
